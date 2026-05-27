28f4b7da06cbb6b25b454ac7a9ef95b7782bb64c
Yanu <59870900+khusanova@users.noreply.github.com>
2026-05-27 13:32:08 +0200

    Add pydantic schema for Ship

---
+++ schemas/ship.py
@@ -0,0 +1,25 @@
+"""Pydantic schema for Ship"""
+
+from datetime import date
+from uuid import UUID
+
+from pydantic import BaseModel, ConfigDict
+
+
+class ShipBase(BaseModel):
+    shipname: str
+    start_date: date
+
+
+class ShipCreate(ShipBase):
+    pass
+
+
+class ShipRead(ShipBase):
+    ship_id: UUID
+    model_config = ConfigDict(from_attributes=True)
+
+
+class ShipUpdate(BaseModel):
+    shipname: str | None = None
+    start_date: date | None = None
