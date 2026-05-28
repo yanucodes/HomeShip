dd7ed8febd57d3c5258950b4103093efc8b71d49
Yanu <59870900+khusanova@users.noreply.github.com>
2026-05-28 14:11:53 +0200

    Update User schema to include display_name field

--- schemas/user.py
+++ schemas/user.py
@@ -1,16 +1,21 @@
 """Pydantic schema for User"""
 
 from uuid import UUID
+from typing import Annotated
+from pydantic import BaseModel, ConfigDict, EmailStr, StringConstraints
 
-from pydantic import BaseModel, ConfigDict, EmailStr
+
+DisplayName = Annotated[str, StringConstraints(min_length=1, max_length=30)]
 
 
 class UserBase(BaseModel):
     username: str
+    display_name: DisplayName
     email: EmailStr
 
 
 class UserCreate(UserBase):
+    display_name: DisplayName | None = None
     password: str
 
 
@@ -19,7 +24,8 @@ class UserRead(UserBase):
     model_config = ConfigDict(from_attributes=True)
 
 
-class UserUpdate(BaseModel):
+class UserUpdate(UserBase):
     username: str | None = None
+    display_name: DisplayName | None = None
     email: EmailStr | None = None
     password: str | None = None
