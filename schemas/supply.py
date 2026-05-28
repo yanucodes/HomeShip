d0fdeb76b8ece72e3226c10cb643f873ae6112a1
Yanu <59870900+khusanova@users.noreply.github.com>
2026-05-28 14:32:21 +0200

    Update pydantic schema for Supply to conform to new database schema

--- schemas/supply.py
+++ schemas/supply.py
@@ -1,29 +1,33 @@
 """Pydantic schema for Supply"""
 
+from datetime import date
 from uuid import UUID
 
 from pydantic import BaseModel, ConfigDict
 
 from models.alert_state import AlertState
+from models.supply import StockState
+
 
 class SupplyBase(BaseModel):
     name: str
+    stock_state: StockState
     quantity: int | None = None
+    date_due: date | None = None
 
 
 class SupplyCreate(SupplyBase):
-    in_stock: bool = True
+    stock_state: StockState = StockState.OUT_OF_STOCK
 
 
 class SupplyRead(SupplyBase):
     supply_id: UUID
     ship_id: UUID
-    in_stock: bool
     alert_state: AlertState
     model_config = ConfigDict(from_attributes=True)
 
 
-class SupplyUpdate(BaseModel):
+class SupplyUpdate(SupplyBase):
     name: str | None = None
-    in_stock: bool | None = None
-    quantity: int | None = None
+    stock_state: StockState | None = None
+
