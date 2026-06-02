8b896bb265d04cc66ec61961db18405cc06fce7e
Yanu <59870900+khusanova@users.noreply.github.com>
2026-06-02 16:40:18 +0200

    Include router users

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 
+from routers import users
+
 app = FastAPI()
+
+app.include_router(users.router)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
