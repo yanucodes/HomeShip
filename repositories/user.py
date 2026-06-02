3f0288a900aff685305932d2a10a114eca0206af
Yanu <59870900+khusanova@users.noreply.github.com>
2026-06-02 19:33:10 +0200

    Add get_by_email to UserRepository

--- repositories/user.py
+++ repositories/user.py
@@ -1,4 +1,6 @@
 """Repository for User."""
+from sqlalchemy import select
+
 from models import User
 
 from .base import BaseRepository
@@ -7,3 +9,16 @@ from .base import BaseRepository
 class UserRepository(BaseRepository[User]):
     """CRUD operations on User model."""
     model = User
+
+    def get_by_email(self, user_email: str) -> User | None:
+        """Fetch user by email.
+
+        Args:
+            user_email: email to search for.
+
+        Returns:
+            User object or None, if user not found.
+        """
+        return self.session.execute(
+            select(User).filter_by(email=user_email)
+        ).scalar_one_or_none()
