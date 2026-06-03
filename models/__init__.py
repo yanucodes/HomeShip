e1f384515384676ef10b30ac9e8bd1f1fee8b294
Yanu <59870900+khusanova@users.noreply.github.com>
2026-06-03 17:12:03 +0200

    Add AlertState to module imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,11 @@
 """Default imports from models module"""
 
-from .base import Base
+from .alert_state import AlertState
 from .user import User
 from .ship import Ship
 from .ship_member import ShipMember
 from .task import Task
 from .supply import Supply
 
-__all__ = ["Base", "User", "Ship", "ShipMember", "Task", "Supply"]
+__all__ = ["AlertState", "User", "Ship", "ShipMember", "Task",
+           "Supply"]
