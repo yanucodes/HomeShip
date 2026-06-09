e6adbc871a66a1cbd5c704e9170edf7f614e73f8
Yanu <59870900+khusanova@users.noreply.github.com>
2026-06-09 12:15:50 +0200

    Add Base in __init__.py for models

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,12 @@
 """Default imports from models module"""
 
 from .alert_state import AlertState
+from .base import Base
 from .user import User
 from .ship import Ship
 from .ship_member import ShipMember
 from .task import Task
 from .supply import Supply
 
-__all__ = ["AlertState", "User", "Ship", "ShipMember", "Task",
+__all__ = ["AlertState", "Base", "User", "Ship", "ShipMember", "Task",
            "Supply"]
