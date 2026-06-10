7df14b4197c7ed159df7e1a8bf73754d2e1cebc6
Yanu <59870900+khusanova@users.noreply.github.com>
2026-06-10 16:43:12 +0200

    Add router ships

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ Creates the app and mounts the HTTP routers.
 """
 from fastapi import FastAPI
 
-from routers import auth, users
+from routers import auth, ships, users
 
 app = FastAPI()
 
 app.include_router(users.router)
+app.include_router(ships.router)
 app.include_router(auth.router)
 
 
