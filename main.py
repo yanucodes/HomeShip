550d6e3038e9fb033f3ad23d3dacd6320d60f451
Yanu <59870900+khusanova@users.noreply.github.com>
2026-06-10 16:42:54 +0200

    Add router auth

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ Creates the app and mounts the HTTP routers.
 """
 from fastapi import FastAPI
 
-from routers import users
+from routers import auth, users
 
 app = FastAPI()
 
 app.include_router(users.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
