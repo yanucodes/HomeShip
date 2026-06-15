71296262b8abf613f7ef293c4707af504a74dbb1
Yanu <59870900+khusanova@users.noreply.github.com>
2026-06-15 19:09:31 +0200

    Set default for JWT algorithm

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ class Settings(BaseSettings):
     database_url: str
     test_database_url: str | None = None
     jwt_secret_key: str
-    jwt_algorithm: str
+    jwt_algorithm: str = "HS256"
     access_token_expire_minutes: int = 60
     default_postpone_days: int = 3
     supply_deadline_red_days: int = 1
